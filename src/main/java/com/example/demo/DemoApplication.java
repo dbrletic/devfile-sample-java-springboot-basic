23826a1450b9765d07866f47404d010223be65b2
Drew Brletich <dbrletic@redhat.com>
2022-03-07 11:33:58 -0500

    Fixed Imports

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @SpringBootApplication
- @RequestMapping("/rest-example")
+@RequestMapping("/rest-example")
 public class DemoApplication {
 
     @RequestMapping("/")
