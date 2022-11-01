1dc8c54506c9685e442bf4eb1eee3e96fd0bcee5
Andrew Brletich <dbrletic@redhat.com>
2022-11-01 14:32:26 -0400

    Removed static logger stuff

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -73,11 +73,6 @@ public class DemoApplication {
     }
 
     public static void main(String[] args) {
-            // assume SLF4J is bound to logback in the current environment
-            LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-            // print logback's internal status
-            StatusPrinter.print(lc);
-
         SpringApplication.run(DemoApplication.class, args);
     }
 
