3c881523751ab4c403eb97f571d4b5c8ccd9b952
Andrew Brletich <dbrletic@redhat.com>
2022-11-03 13:35:25 -0400

    Write transaction to a specific file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,13 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.core.util.StatusPrinter;
-
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,9 +16,7 @@ import org.slf4j.LoggerFactory;
 public class DemoApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
-	//private static FluentLogger LOG = FluentLogger.getLogger("transaction_log.ng.double.encoded");
-    //private static FluentLogger LOGMANUAL = FluentLogger.getLogger("transaction_log.ng.double.encoded", "cloudwatch-openshift-logforwarding-cloudwatch.openshift-logging.svc", 24224);
-    //private static FluentLogger LOGLOCAL = FluentLogger.getLogger("transaction_log.ng.double.encoded", "localhost", 24224);
+    private static final Logger transactionLogger = LoggerFactory.getLogger("transactionLogger");
         
     @RequestMapping("/")
     String home() {
@@ -33,49 +26,26 @@ public class DemoApplication {
         LOG.info("This will be printed on info");
         LOG.warn("This will be printed on warn");
         LOG.error("This will be printed on error");
-        System.out.println("Starting FluentLogger through logback");
-       /*  Map<String, Object> data = new HashMap<String, Object>();
-        data.put("from", "userA");
-        data.put("to", "userB");
-        LOG.log("follow", data); */
         return "Hello World! Logging information now through logback.xml to remote servergi";
     }
-    /*
-    @RequestMapping("/manual")
-    String manual(){
-        System.out.println("Starting FluentLogger through manaul connection to remote server");
-        logger.info("Starting the manual logger");
-        Map<String, Object> data = new HashMap<String, Object>();
-        data.put("from", "userA");
-        data.put("to", "userB");
-        LOGMANUAL.log("follow", data);
-        LOGMANUAL.flush();
-        return "Hello World! Logging information now through manual connection";
-    }
-
-    @RequestMapping("/local")
-    String local(){
-        //Just to see if the java code will bomb out with a connection not running
-        System.out.println("Starting FluentLogger through local host");
-        logger.info("Starting the local logger");
-        Map<String, Object> data = new HashMap<String, Object>();
-        data.put("from", "userA");
-        data.put("to", "userB");
-        LOGLOCAL.log("follow", data);
-        LOGLOCAL.flush();
-        return "Hello World! Logging information now through local host";
-    }
-     */
-   @RequestMapping(value = "/{name}")
-   String homeName(@PathVariable String name) {
-    LOG.trace("This will be printed on trace for " + name);
-    LOG.debug("This will be printed on debug for " + name);
-    LOG.info("This will be printed on info for " + name);
-    LOG.warn("This will be printed on warn for " + name);
-    LOG.error("This will be printed on error for " + name);
+    @RequestMapping(value = "/{name}")
+    String homeName(@PathVariable String name) {
+        LOG.trace("This will be printed on trace for " + name);
+        LOG.debug("This will be printed on debug for " + name);
+        LOG.info("This will be printed on info for " + name);
+        LOG.warn("This will be printed on warn for " + name);
+        LOG.error("This will be printed on error for " + name);
         return "Hello " + name + "! Log files have been printed for you";
     }
-
+    @RequestMapping(value = "/transaction/{name}")
+    String transactionName(@PathVariable String name) {
+        transactionLogger.trace("This will be printed on trace for " + name + " in the transaction.log");
+        transactionLogger.debug("This will be printed on debug for " + name + " in the transaction.log");
+        transactionLogger.info("This will be printed on info for " + name + " in the transaction.log");
+        transactionLogger.warn("This will be printed on warn for " + name + " in the transaction.log");
+        transactionLogger.error("This will be printed on error for " + name + " in the transaction.log");
+        return "Hello " + name + "! Transaction Log files have been printed for you";
+    }
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
